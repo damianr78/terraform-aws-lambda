61a35f909c31cebee633cebbe0d9a0f8e87b1fbf
Julian Calvento <julian.calvento@10pines.com>
2019-09-03 11:45:36 -0300

    Alias invoke arn/name (#11)
    Add alias invoke arn and name to outputs

--- outputs.tf
+++ outputs.tf
@@ -10,3 +10,12 @@ output "invoke_arn" {
   value = local.invoke_arn
 }
 
+output "alias_invoke_arn" {
+  value = aws_lambda_alias.alias.invoke_arn
+}
+
+output "alias_name" {
+  description = "Used for API Gateway permission to allow lambda invocations"
+  value = "${module.lambda-label.function_name}:${aws_lambda_alias.alias.name}"
+}
+
