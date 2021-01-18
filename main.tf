310810191e9648962f0fcbe90a2186b4b416ec60
mariano.sanfilippo <mariano.sanfilippo@uala.com.ar>
2021-01-18 16:21:18 -0300

    fix in functionName

--- main.tf
+++ main.tf
@@ -156,7 +156,7 @@ resource "aws_lambda_event_source_mapping" "dynamodb_triggers" {
   bisect_batch_on_function_error     = lookup(each.value, "bisect_batch_on_function_error", null)
   enabled                            = lookup(each.value, "enabled", null)
   event_source_arn                   = lookup(each.value, "event_source_arn", null)
-  function_name                      = concat(aws_lambda_function.lambda.arn, ":", module.lambda-label.environment_upper)
+  function_name                      = "${aws_lambda_function.lambda.arn}:${module.lambda-label.environment_upper}"
   parallelization_factor             = lookup(each.value, "parallelization_factor", null)
   starting_position                  = lookup(each.value, "starting_position", null)
 }
